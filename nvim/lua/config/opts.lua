67dc02d1515588f58fb2b33d342425b1fb16dc49
Xavi Grimau <x.grimauu@gmail.com>
2024-01-28 22:28:41 +0100

    plugins

---
+++ nvim/lua/config/opts.lua
@@ -0,0 +1,21 @@
+vim.cmd[[colorscheme tokyonight]]
+
+-- This file is automatically loaded by plugins.core
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+local opt = vim.opt
+
+opt.autowrite = true -- Enable auto write
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
+opt.cursorline = true -- Enable highlighting of the current line
+opt.expandtab = true -- Use spaces instead of tabs
+
+opt.list = true -- Show some invisible characters (tabs...
+opt.mouse = "a" -- Enable mouse mode
+opt.number = true -- Print line number
+opt.list = true -- Show some invisible characters (tabs...
+opt.mouse = "a" -- Enable mouse mode
+opt.number = true -- Print line number
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.termguicolors = true -- True color support
