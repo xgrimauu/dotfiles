ffde2f89242beb31d2edb0d5e29a08e419064ed8
Xavi Grimau <x.grimauu@gmail.com>
2024-02-07 22:32:01 +0100

    lsp

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,20 +1,27 @@
 return {
-		"williamboman/mason.nvim",
-		cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
-		build = ":MasonUpdate",
-        		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-		},
-	 	config = function()
-		    	require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "",
-						package_pending = "",
-						package_uninstalled = "",
-					},
-				}
-			})
-			require("mason-lspconfig").setup()
-	 	end,
+	"williamboman/mason.nvim",
+	cmd = {
+		"Mason",
+		"MasonInstall",
+		"MasonUpdate",
+		"MasonInstallAll",
+		"MasonUninstall",
+		"MasonUninstallAll",
+		"MasonLog",
+	},
+	build = "MasonUpdate",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		require("mason").setup({
+			ui = {
+				icons = {
+					package_installed = "",
+					package_pending = "",
+					package_uninstalled = "",
+				},
+			},
+		})
+	end,
 }
