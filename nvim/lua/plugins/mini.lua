b7f1d80d7e69c00ce97482ef9483f758383bba12
Xavi Grimau <x.grimauu@gmail.com>
2024-02-09 18:33:55 +0100

    seems working

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,22 +1,14 @@
-require("mini.ai").setup({})
-require("mini.pairs").setup({})
-
 return {
-	{ "echasnovski/mini.nvim", version = "*" },
-	{ "echasnovski/mini.pairs", version = "*" },
-	{ "echasnovski/mini.ai", version = "*" },
 	{
-		"echasnovski/mini.surround",
-		opts = {
-			mappings = {
-				add = "gza", -- Add surrounding in Normal and Visual modes
-				delete = "gzd", -- Delete surrounding
-				find = "gzf", -- Find surrounding (to the right)
-				find_left = "gzF", -- Find surrounding (to the left)
-				highlight = "gzh", -- Highlight surrounding
-				replace = "gzr", -- Replace surrounding
-				update_n_lines = "gzn", -- Update `n_lines`
-			},
+		"echasnovski/mini.nvim",
+		dependencies = {
+			{ "echasnovski/mini.ai", version = "*" },
+			{ "echasnovski/mini.pairs", version = "*" },
 		},
+		version = "*",
+		config = function()
+			require("mini.ai").setup({})
+			require("mini.pairs").setup({})
+		end,
 	},
 }
